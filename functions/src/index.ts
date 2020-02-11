9c1d33bbc9faecd2e783ba15b2fc5daebf1b297f
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-02-11 23:00:42 +0900

    Refactor to use async functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,42 +9,36 @@ type Event = {
   eventUrl: string;
 };
 
-const fetchConnpass = (): Promise<Event[]> => {
-  return axios
-    .get(
-      "https://connpass.com/api/v1/event/?keyword_or=ruby&keyword_or=javascript&order=2&count=25"
-    )
-    .then(res => {
-      return res.data.events.map((d: any) => {
-        return {
-          site: "connpass",
-          title: d.title,
-          address: d.address,
-          startedAt: d.started_at,
-          eventUrl: d.event_url
-        };
-      });
-    });
+const fetchConnpass = async (): Promise<Event[]> => {
+  const res = await axios.get(
+    "https://connpass.com/api/v1/event/?keyword_or=ruby&keyword_or=javascript&order=2&count=25"
+  );
+  return res.data.events.map((d: any) => {
+    return {
+      site: "connpass",
+      title: d.title,
+      address: d.address,
+      startedAt: d.started_at,
+      eventUrl: d.event_url
+    };
+  });
 };
 
-const fetchDoorkeeper = (): Promise<Event[]> => {
-  return axios
-    .get("https://api.doorkeeper.jp/events", {
-      // test token
-      headers: { Authorization: "Bearer LmzURzyT32EcBifywVPT" }
-    })
-    .then(res => {
-      return res.data.map((d: any) => {
-        const event = d.event;
-        return {
-          site: "doorkeeper",
-          title: event.title,
-          address: event.address,
-          startedAt: event.starts_at,
-          eventUrl: event.public_url
-        };
-      });
-    });
+const fetchDoorkeeper = async (): Promise<Event[]> => {
+  const res = await axios.get("https://api.doorkeeper.jp/events", {
+    // test token
+    headers: { Authorization: "Bearer LmzURzyT32EcBifywVPT" }
+  });
+  return res.data.map((d: any) => {
+    const event = d.event;
+    return {
+      site: "doorkeeper",
+      title: event.title,
+      address: event.address,
+      startedAt: event.starts_at,
+      eventUrl: event.public_url
+    };
+  });
 };
 
 export const queryEvents = functions.https.onRequest((request, response) => {
