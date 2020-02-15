6cbbb84e248ead0ec711b61a46a40363a3101e15
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-02-15 18:45:50 +0900

    Fix endpoint to only path

--- src/containers/Main.tsx
+++ src/containers/Main.tsx
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import { List } from "../components/List";
 import { Event } from "../../api/events";
 
+// fetch の引数の url は path だけを指定しても勝手に origin を補完してくれる
+// see. https://github.github.io/fetch/#url
+const eventsApiEndpoint = "/api/events";
+
 export const Main = () => {
   const [data, setData] = useState<Event[]>([]);
   useEffect(() => {
-    fetch(`${document.location.origin}/api/events`)
+    fetch(eventsApiEndpoint)
       .then(res => {
         return res.json();
       })
