4346d63fabb2bf0e5c67f73a1b150b85dbb04e6c
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-02-24 23:11:31 +0900

    Introduce SearchForm with react-hook-form

--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -3,15 +3,18 @@ import styles from "./index.module.scss";
 
 export type Props = {
   onChange: (_: any) => void;
+  register: () => void;
   min?: string;
 };
 
-export const DatePicker: React.FC<Props> = ({ onChange, min }) => {
+export const DatePicker: React.FC<Props> = ({ onChange, register, min }) => {
   return (
     <div>
       <input
         className={styles.myForm}
         type="date"
+        name="date"
+        ref={register}
         onChange={onChange}
         min={min}
       />
