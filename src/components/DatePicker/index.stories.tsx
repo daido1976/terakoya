5031c6a9fb5caea11e3bcbbecacc34849df11e22
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-02-24 16:36:28 +0900

    Add min to DatePicker as option args

--- src/components/DatePicker/index.stories.tsx
+++ src/components/DatePicker/index.stories.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
+import { todayKebabCase } from "../../utils/time";
 import { DatePicker } from ".";
 
 storiesOf("DatePicker", module).add("default", () => (
-  <DatePicker onChange={action("onChange!")}></DatePicker>
+  <DatePicker
+    onChange={action("onChange!")}
+    min={todayKebabCase()}
+  ></DatePicker>
 ));
--- src/components/DatePicker/index.tsx
+++ src/components/DatePicker/index.tsx
@@ -3,12 +3,18 @@ import styles from "./index.module.scss";
 
 export type Props = {
   onChange: (_: any) => void;
+  min?: string;
 };
 
-export const DatePicker: React.FC<Props> = ({ onChange }) => {
+export const DatePicker: React.FC<Props> = ({ onChange, min }) => {
   return (
     <div>
-      <input className={styles.myForm} type="date" onChange={onChange} />
+      <input
+        className={styles.myForm}
+        type="date"
+        onChange={onChange}
+        min={min}
+      />
     </div>
   );
 };
