ec242574153ca44501de21ad8f9fea2f449654bf
Daido Shota <n_i_x_o_n777@yahoo.co.jp>
2020-02-25 00:24:43 +0900

    Display SearchForm when clicks icon

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./index.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { SearchForm } from "../SearchForm";
 
 export type Props = {
   onSearch: (_: any) => void;
 };
 
 export const Header: React.FC<Props> = ({ onSearch }) => {
+  const [showForm, onToggle] = useState(false);
+
   return (
-    <div className={styles.myHeader}>
-      <div className={styles.myHeaderTitle}>terakoya</div>
-      <div className={styles.myHeaderSearch} onClick={() => {}}>
-        <FontAwesomeIcon icon={faSearch} />
+    <div>
+      <div className={styles.myHeader}>
+        <div className={styles.myHeaderTitle}>terakoya</div>
+        <div
+          className={styles.myHeaderSearch}
+          onClick={() => onToggle(prev => (prev ? false : true))}
+        >
+          <FontAwesomeIcon icon={faSearch} />
+        </div>
+      </div>
+      <div className={styles.myForm}>
+        {showForm ? <SearchForm onSearch={onSearch}></SearchForm> : null}
       </div>
     </div>
   );
